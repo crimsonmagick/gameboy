e57f019f1b07c48719934f1022e950cea18c565f
Welby Seely <welbyseely@gmail.com>
2022-12-23 21:24:10 -0500

    Added gradle and Spock

---
+++ src/test/groovy/com/mangomelancholy/gameboy/emulator/GameBoyEmulatorTest.groovy
@@ -0,0 +1,10 @@
+package com.mangomelancholy.gameboy.emulator
+
+import spock.lang.Specification
+
+class GameBoyEmulatorTest extends Specification {
+    def "dummy test"() {
+        given:
+        def app = new GameBoyEmulator()
+    }
+}
